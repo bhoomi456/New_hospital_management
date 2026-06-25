2143276382cd30eddc55b0b8f6ade84d38900962
Bhoomi jain <bhoomij0304@gmail.com>
2026-06-25 12:51:36 +0530

    Patient (#22)
    * Created new endpoint for patient
    
    * Created create endpoint for patient

--- app/controllers/patients.controller.rb
+++ app/controllers/patients.controller.rb
@@ -2,4 +2,22 @@ class PatientsController < ApplicationController
   def index
     @patients = Patient.all
   end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @patient = Patient.new(patient_params)
+    if @patient.save
+      redirect_to patients_path, notice: "Patient Added Successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  def patient_params
+    params.require(:patient).permit(:name, :age, :gender)
+  end
 end
