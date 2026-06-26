0d2272df92783185b7791ed8511dc278868d5929
Bhoomi jain <bhoomij0304@gmail.com>
2026-06-26 15:30:16 +0530

    Created new and create endpoint in profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,23 @@ class ProfilesController < ApplicationController
   def index
     @profiles = Profile.all
   end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to profiles_path, notice: "Profile Added Successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+
+  def profile_params
+    params.require(:profile).permit(:consultation_fee, :doctor_id, :experience)
+  end
 end
