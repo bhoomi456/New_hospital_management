f4fb1dec42ad680001259a294cdb433ce3bef935
Bhoomi jain <bhoomij0304@gmail.com>
2026-07-24 13:35:45 +0530

    Show changes before and after in mail

--- app/mailers/hospital_mailer.rb
+++ app/mailers/hospital_mailer.rb
@@ -1,9 +1,13 @@
 class HospitalMailer < ApplicationMailer
   def hospital_updated(hospital)
     @hospital = hospital
+
+    changes = @hospital.previous_changes
+
     mail(to: @hospital.admin_email, 
       subject: "Hospital details updated",
-      body: "The hospital details have been updated."
+      body: "Name: #{changes['name'][0]} <- Updated To -> #{changes['name'][1]}\n" \
+            "Address: #{changes['address'][0]} <- Updated To -> #{changes['address'][1]}"
     )
   end
 end
