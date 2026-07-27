ed56aa53141d6d299bad756cad9cfb23eb443c19
Bhoomi jain <bhoomij0304@gmail.com>
2026-07-27 17:22:18 +0530

    Created 30 lakh hospital by using Active record import gem (#69)
    * Install Active record import gem
    
    * Created 30 lakh hospital in batches using active record import gem
    
    * Display count of each batches
    
    * Calculate batch time

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+TOTAL = 3000000
+BATCH_SIZE = 30000
+imported_count = 0
 
-1000.times do
-  Hospital.create!(name: "#{Faker::Company.name} Hospital",
+hospitals = []
+start_time = Time.now
+
+TOTAL.times do
+  hospitals << Hospital.new(
+    name: "#{Faker::Address.city} Hospital",
     admin_email: Faker::Internet.unique.email,
     address: Faker::Address.full_address
   )
+
+  if hospitals.size == BATCH_SIZE
+    batch_start = Time.now
+
+    Hospital.import(hospitals)
+    batch_end = Time.now
+    imported_count += hospitals.size
+    puts "Imported #{imported_count} hospitals | Batch Time: #{batch_end - batch_start} sec"
+    hospitals.clear
+  end
 end
+
+Hospital.import(hospitals) if hospitals.any?
+puts "Total Time: #{Time.now - start_time} sec"
