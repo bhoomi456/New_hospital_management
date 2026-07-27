b1eb46ff1c907b179e46c7809b7a6a7c55c1a974
Bhoomi jain <bhoomij0304@gmail.com>
2026-07-27 12:02:02 +0530

    Created 30 lakh hospital in batches using active record import gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+TOTAL = 3000000
+BATCH_SIZE = 300
 
-1000.times do
-  Hospital.create!(name: "#{Faker::Company.name} Hospital",
+hospitals = []
+
+
+TOTAL.times do
+  hospitals << Hospital.new(
+    name: "#{Faker::City.name} Hospital",
     admin_email: Faker::Internet.unique.email,
     address: Faker::Address.full_address
   )
+
+  if hospitals.size == BATCH_SIZE
+    Hospital.import(hospitals)
+    hospitals.clear
+  end
 end
+
+Hospital.import(hospitals) if hospitals.any?
